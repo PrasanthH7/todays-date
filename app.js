569a0515bdcccd9cad6287525f0e7b96ae567ad5
PrasanthH7 <prasanthkumarbotcha@gmail.com>
2023-05-05 20:12:00 +0000

    error1testcasefailed

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  const todaysDate = new Date();
+  response.send(
+    `${todaysDate.getDate()}-${
+      todaysDate.getMonth() + 1
+    }-${todaysDate.getYear()}`
+  );
+});
+
+module.exports = app;
